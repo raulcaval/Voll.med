fd16b90d8b119014e87b901678407775865585f0
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-05-20 16:43:29 -0300

    Alteração do metodo de listagem para listar com paginação

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -6,6 +6,8 @@ import med.voll.api.medico.DadosListagemMedico;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,9 +27,15 @@ public class MedicoController {
     }
 
     @GetMapping
-    public List<DadosListagemMedico> listar() {
+    public Page<DadosListagemMedico> listar(Pageable paginacao) {
 //        Endender tudo o que essa linha faz.
-        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+        return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
+//    @GetMapping
+//    public List<DadosListagemMedico> listar() {
+////        Endender tudo o que essa linha faz.
+//        return repository.findAll().stream().map(DadosListagemMedico::new).toList();
+//    }
+    
 }
