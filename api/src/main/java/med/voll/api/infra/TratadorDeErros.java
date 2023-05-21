4eedcc5f043d32ae54b0409152807f22775d56a7
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-05-21 20:15:46 -0300

    implementação de classe para dtrataentod e erros, configuração para inibir o stacktrace no retorno da requisição.

---
+++ api/src/main/java/med/voll/api/infra/TratadorDeErros.java
@@ -0,0 +1,38 @@
+package med.voll.api.infra;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+
+    @ExceptionHandler( EntityNotFoundException.class ) //notfound
+    public ResponseEntity tratarErro404() {
+        return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler( MethodArgumentNotValidException.class ) // campo inválido, badRequest
+    public ResponseEntity tratarErro400( MethodArgumentNotValidException ex) {
+        var erros = ex.getFieldErrors(); //recebe lista de erros
+        return ResponseEntity.badRequest().body(erros.stream().map(DadosErrosValidacao::new).toString());
+    }
+
+
+    private record DadosErrosValidacao(
+            String campo,
+            String mensagem ) {
+        public DadosErrosValidacao(FieldError erro) {
+            this(
+                    erro.getField(),
+                    erro.getDefaultMessage()
+            );
+        }
+
+    }
+
+}
+
