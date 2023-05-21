7f9c066cede8c9227d19a976649700ee14f18c49
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-05-20 22:24:25 -0300

    reafatoração da estrutura do projeto, codigos de paciente.

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,15 +1,13 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.*;
+import med.voll.api.domain.medico.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -25,7 +23,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> listar( @PageableDefault( size = 10, sort = {"nome"} ) Pageable paginacao ) {
+    public Page<DadosListagemMedico> listar(@PageableDefault( size = 10, sort = {"nome"} ) Pageable paginacao ) {
         return repository.findAllByAtivoTrue(paginacao).map(DadosListagemMedico::new);
     }
 
--- api/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
+++ api/src/main/java/med/voll/api/domain/paciente/PacienteRepository.java
@@ -1,11 +1,10 @@
-package med.voll.api.medico;
+package med.voll.api.domain.paciente;
 
 import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
-
-    Page<Medico> findAllByAtivoTrue(Pageable paginacao);
+public interface PacienteRepository extends JpaRepository<Paciente, Long> {
+    Page<Paciente> findAllByAtivoTrue(Pageable paginacao);
 }
