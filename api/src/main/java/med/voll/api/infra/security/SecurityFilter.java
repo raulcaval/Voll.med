a03fa6fafaa466b7abfa58a5e190cd3dad43014a
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-06-06 14:10:52 -0300

    implementará a validação e recuperação do token JWT.

---
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,40 @@
+package med.voll.api.infra.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    @Autowired
+    TokenService tokenService;
+
+    @Override
+    protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
+    ) throws ServletException, IOException
+    {
+        var tokenJWT = recuperarToken( request );
+        var subject = tokenService.getSubject( tokenJWT );
+        System.out.println(subject);
+        filterChain.doFilter( request,response );
+    }
+
+    private String recuperarToken( HttpServletRequest request
+    )
+    {
+        var autorizationHeader = request.getHeader("Authorization");
+        if ( autorizationHeader == null ) {
+            throw new RuntimeException("Token JWT não ennviado no cabeçalho Authorization!");
+        }
+        return autorizationHeader.replace("Bearer", "").trim();
+    }
+
+
+}
