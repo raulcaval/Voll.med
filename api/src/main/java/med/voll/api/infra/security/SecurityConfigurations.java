f913c941ed890673145f8a7900ad4f391da84721
Raul Cavalcanti <raul.a.s.cavalcanti@gmail.com>
2023-06-07 12:06:07 -0300

    Implementará a autenticação do usuário, delegando a checagem para o Spring.

--- api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityConfigurations.java
@@ -2,6 +2,7 @@ package med.voll.api.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,26 +16,29 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfigurations {
 
-    //SecurityFilterChain: Objeto spring para configuraçôes de autenticação e tb autorização
-    // @Bean: expoe o retorno do metodo, retornando o objeto para o spring.
     @Bean
-    public SecurityFilterChain securityFilterChain( HttpSecurity http ) throws Exception {
-        /*
-        http.csrf().disable(): Desabilita o csrf
-        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS): Desabilita processo de autenticação padrão
-        * */
+    public SecurityFilterChain securityFilterChain( HttpSecurity http
+    ) throws Exception
+    {
         return http.csrf().disable()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .sessionManagement().sessionCreationPolicy( SessionCreationPolicy.STATELESS )
+                .and().authorizeHttpRequests()
+                .requestMatchers( HttpMethod.POST, "/login" ).permitAll()
+                .anyRequest().authenticated()
                 .and().build();
     };
 
     @Bean
-    public AuthenticationManager authenticationManager ( AuthenticationConfiguration configuration ) throws Exception {
+    public AuthenticationManager authenticationManager ( AuthenticationConfiguration configuration
+    ) throws Exception
+    {
         return configuration.getAuthenticationManager();
     }
 
     @Bean
-    public PasswordEncoder passwordEncoder () {
+    public PasswordEncoder passwordEncoder (
+    )
+    {
         return new BCryptPasswordEncoder();
     }
 }
--- api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ api/src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -4,7 +4,10 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import med.voll.api.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,13 +19,24 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     TokenService tokenService;
 
+    @Autowired
+    UsuarioRepository repository;
+
     @Override
     protected void doFilterInternal( HttpServletRequest request, HttpServletResponse response, FilterChain filterChain
     ) throws ServletException, IOException
     {
         var tokenJWT = recuperarToken( request );
-        var subject = tokenService.getSubject( tokenJWT );
-        System.out.println(subject);
+
+        if ( tokenJWT != null ) {
+            var subject = tokenService.getSubject( tokenJWT );
+            var usuario = repository.findByLogin( subject );
+
+            var authentication = new UsernamePasswordAuthenticationToken( usuario, null, usuario.getAuthorities() );
+
+            SecurityContextHolder.getContext().setAuthentication( authentication );
+        }
+
         filterChain.doFilter( request,response );
     }
 
@@ -30,11 +44,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     )
     {
         var autorizationHeader = request.getHeader("Authorization");
-        if ( autorizationHeader == null ) {
-            throw new RuntimeException("Token JWT não ennviado no cabeçalho Authorization!");
+        if ( autorizationHeader != null ) {
+            return autorizationHeader.replace("Bearer", "" ).trim();
         }
-        return autorizationHeader.replace("Bearer", "").trim();
+        return null;
     }
-
-
 }
